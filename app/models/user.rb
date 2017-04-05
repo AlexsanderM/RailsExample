db82e8a413c9ae5e4f6c2747c04174d1dfcbc5c0
AlexsanderM <malov.alexsander@yandex.ru>
2017-04-05 12:21:34 +0300

    edit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX } ,
                     uniqueness: true # :unique option to the validates method
                                      # enforce uniqueness of email addresses
+
+  has_secure_password
+    # As long as there is a password_digest column 
+    #in the database, adding this one method to our model
+    #gives us a secure way to create and authenticate new users.
+  validates :password, length: { minimum: 6 }
+
 end
