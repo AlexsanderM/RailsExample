83e691ebfd0c57344e904544e840da10e3410536
AlexsanderM <malov.alexsander@yandex.ru>
2017-04-06 19:49:53 +0300

    add create in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params) # @user = User.new(params[:user])
+    if @user.save
+
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_parems
+      params.require(:user).permit( :name, :email, :password,
+                                    :password_confirmation)      
+    end
+
 end
